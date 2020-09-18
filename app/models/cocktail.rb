48374b28b2fccb0450092814ae6d83155102bfa0
Diogo Martins-de-Sa <sadiogo@mol.bio.br>
2020-09-18 15:08:55 -0300

    Validations applied

---
+++ app/models/cocktail.rb
@@ -0,0 +1,17 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses, before_remove: :cocktail_independent_of?
+
+  validates :name, uniqueness: true, presence: true
+
+  private
+
+  def cocktail_independent_of?(ingredient)
+    verification = true
+    unless ingredient.cocktails.nil?
+      self.errors[:deletion_status] = 'Cannot delete an ingredient present in active cocktails'
+      verification = false
+    end
+    verification
+  end
+end
